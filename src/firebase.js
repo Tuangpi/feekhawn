3bf96caeaab32b21ebebd133dec536a24a0838e9
Tuangpi <singkhantuang97@gmail.com>
2022-11-11 23:14:47 +0630

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 const config = {
-  apiKey: "AIzaSyDrULOx0Y934AYED63_afyilaQ8ZTTyrxo",
-  authDomain: "react-tuto-e4657.firebaseapp.com",
-  projectId: "react-tuto-e4657",
-  storageBucket: "react-tuto-e4657.appspot.com",
-  messagingSenderId: "632818805785",
-  appId: "1:632818805785:web:4fc1d27c8a89802ec5ce88",
-  databaseURL: "react-tuto-e4657.firebasedatabase.app",
+  apiKey: "AIzaSyAk-ANcufrwoTJjvBSNG8T7spiMxpvgGiY",
+  authDomain: "fee-khawn.firebaseapp.com",
+  projectId: "fee-khawn",
+  storageBucket: "fee-khawn.appspot.com",
+  messagingSenderId: "472457010871",
+  appId: "1:472457010871:web:6f26a96b0f631a4e433732",
 };
 const app = initializeApp(config);
 const db = getFirestore(app);
